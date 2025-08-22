68b097e94ffb9f18989c0a51ebfaadbbb5049121
Alex <stark.11raj@gmail.com>
2025-08-22 19:33:49 +0300

    Удалил DynamicPivot

--- TransStarterTest/ViewModels/DynamicPivotColumn.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TransStarterTest.ViewModels
-{
-    public class DynamicPivotColumn
-    {
-        public string Header { get; set; }
-        public string Key { get; set; }
-    }
-}
--- TransStarterTest/ViewModels/DynamicPivotRow.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TransStarterTest.ViewModels
-{
-    public class DynamicPivotRow
-    {
-        public string RowKey { get; set; }
-        public Dictionary<string, double> Values { get; set; } = new();
-    }
-}
